007ecccd7a3c1205563479b67618536ed84526e7
sesame-goma <wataru.tatsuda@gmail.com>
2020-08-16 16:49:25 +0900

    タイトルタグを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 // アプリケーション全体のラッパー
 // SSRをきちんとするためには、これが必須
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <title>Wataru Tatsuda Portfolio</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
+)
 
 export default App
